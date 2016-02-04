037522f5a20594c33b115c1b84005d766f150a76
703s <703s@localhost.localdomain>
2016-02-04 14:03:07 +0900

    admin作成開始

---
+++ admin/confirm.php
@@ -0,0 +1,32 @@
+<?php
+require_once(__DIR__.'/lib/functions.php');
+$title=h($_POST['title']);
+$content=strip_tags(nl2br($_POST['content']),'<br>');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link type="text/css" rel="stylesheet" href="admin.css">
+<title>確認</title>
+</head>
+<body>
+<div class="container">
+    <h1 class="maintitle">投稿管理画面</h1>
+    <div class="content">
+       <h2 class="subtitle">投稿内容確認</h2>
+       <h3>投稿タイトル</h3>
+       <p><?php echo $title; ?></p>
+       <h3>本文</h3>
+       <p><?php echo $content; ?></p>
+       <button onClick="history.back();">戻る</button>
+
+        
+    </div>
+
+    
+            
+</div>
+</body>
+</html>
